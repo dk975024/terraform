8e4e05ae1185fce61f46fcbe7f20039fde6fd224
dk975024 <43377507+dk975024@users.noreply.github.com>
2023-03-17 20:16:51 +0530

    Update Jenkinsfile.tf

--- Jenkinsfile.tf
+++ Jenkinsfile.tf
@@ -1,9 +1,16 @@
 pipeline {
     agent any
+    environment {
+    PATH = "${PATH}:${getTerraformPath{}}"
+    }
     stages{
-        stage{'terraform init'}
+        stage('terraform init'){
         steps{
             sh "terraform init"
         }
     }
 }
+def getTerraformPath(){
+  def tfHome = tool name: 'terraform', type: 'terraform'
+  return tfHome 
+ }
